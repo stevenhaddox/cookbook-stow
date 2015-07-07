57257f7b67b5624a892ba31b09fcf9127a9a96f7
Steven Haddox <steven@haddox.us>
2015-07-07 13:13:33 -0400

    Add coveralls

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'steven.haddox@gmail.com'
 license 'all_rights'
 description 'Installs GNU Stow'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
 
 supports 'debian'
 supports 'ubuntu'
