9ab2a205cadd73b45f69100eeef7bebdff2c60c7
Steven Haddox <steven@haddox.us>
2015-07-07 15:35:44 -0400

    Add recipe to metadata

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'steven.haddox@gmail.com'
 license 'all_rights'
 description 'Installs GNU Stow'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.1'
+version '0.1.2'
 
 supports 'debian'
 supports 'ubuntu'
@@ -16,3 +16,5 @@ supports 'redhat'
 depends 'build-essential', '~> 2.2'
 depends 'tar', '~> 0.6.0'
 depends 'yum-epel', '~> 0.6.2'
+
+recipe 'stow::default', 'Install GNU stow via package or source installation'
