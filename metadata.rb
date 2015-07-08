8fa78a9fd21db057661b6c98e68ead705169aa23
Steven Haddox <steven@haddox.us>
2015-07-08 00:27:17 -0400

    0.1.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'steven.haddox@gmail.com'
 license 'all_rights'
 description 'Installs GNU Stow'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.2'
+version '0.1.3'
 
 supports 'debian'
 supports 'ubuntu'
