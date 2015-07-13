b1156b01d9b7ee6ccfceb39a03d7050c60c2fc0b
Steven Haddox <steven@haddox.us>
2015-07-13 11:32:55 -0400

    rubocop refactor

--- providers/package.rb
+++ providers/package.rb
@@ -1,35 +1,38 @@
+# Simplify variable existence checks with .blank?
+def blank?
+  self.nil? || self.empty?
+end
+
 # Stow target path
 def stow_target
-  stow_target = nil
-  if node['stow']['target'] && !node['stow']['target'].empty?
-    stow_target = node['stow']['target']
-  end
-  stow_target
+  node['stow']['target'].blank? ? nil : node['stow']['target']
 end
 
 # Stow directory path
 def stow_path
-  stow_path = nil
-  if node['stow']['path'] && !node['stow']['path'].empty?
-    stow_path = node['stow']['path']
-  end
-  stow_path
+  node['stow']['path'].blank? ? nil : node['stow']['path']
+end
+
+# Stow target bin file check
+def stow_target_bin_exists?
+  ::File.exist?("#{stow_target}/bin/stow")
+end
+
+# Stow path bin file check
+def stow_path_bin_exists?
+  ::File.exist?("#{stow_path}/../bin/stow")
 end
 
 # Detect which stow command to invoke
 # Order of precedence: -t flag > -d flag > 'stow'
 def stow_command
-  command = ''
-  if stow_target && !stow_target.empty?
-    if ::File.exists?("#{stow_target}/bin/stow")
-      command = "#{stow_target}/bin/stow"
-    end
-  elsif stow_path && !stow_path.empty?
-    if ::File.exists?("#{stow_path}/../bin/stow")
-      command = "#{stow_path}/../bin/stow"
-    end
+  if !stow_target.blank? && stow_target_bin_exists?
+    command = "#{stow_target}/bin/stow"
+  elsif !stow_path.blank? && stow_path_bin_exists?
+    command = "#{stow_path}/../bin/stow"
   end
-  command = command.nil? ? 'stow' : command
+
+  command.nil? ? 'stow' : command
 end
 
 # Set stow command flags
@@ -37,10 +40,21 @@ def stow_command_flags
   flags  = ''
   flags += "-t #{stow_target}" unless stow_target.nil?
   flags += "-d #{stow_path}" unless stow_path.nil?
+  flags
 end
 
 def stow
   "#{stow_command} #{stow_command_flags}"
+end
+
+# Destow all package directories with "#{name}-" as a prefix
+def destow_existing
+  # TODO
+end
+
+# Destow specified current version
+def destow_current_version
+  # TODO
 end
 
 action :stow do
@@ -49,9 +63,9 @@ action :stow do
 
   # Destow existing > current version as latter is included in the former
   if @new_resource.destow_existing == true
-    # Destow all package directories with "#{name}-" as a prefix
-  elsif @new_resource.current_version && !@new_resource.current_version.empty?
-    # Destow specified current version
+    destow_existing
+  elsif !@new_resource.current_version.blank?
+    destow_current_version
   end
 
   execute "stow_#{name}-#{version}" do
--- resources/package.rb
+++ resources/package.rb
@@ -4,6 +4,6 @@ default_action :stow
 attribute :name, kind_of: String, required: true, name_attribute: true
 attribute :version, kind_of: String, required: true
 attribute :current_version, kind_of: [String, NilClass], required: false,
-  default: nil
+                            default: nil
 attribute :destow_existing, kind_of: [TrueClass, FalseClass], required: false,
-  default: false
+                            default: false
