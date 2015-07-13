b4017070d5879dae8dc63a3b0d2deb5b1819d41d
Steven Haddox <steven@haddox.us>
2015-07-13 11:37:51 -0400

    foodcritic told me to notify on update :)

--- providers/package.rb
+++ providers/package.rb
@@ -58,26 +58,30 @@ def destow_current_version
 end
 
 action :stow do
-  name = @new_resource.name
-  version = @new_resource.version
+  name = new_resource.name
+  version = new_resource.version
 
   # Destow existing > current version as latter is included in the former
-  if @new_resource.destow_existing == true
+  if new_resource.destow_existing == true
     destow_existing
-  elsif !@new_resource.current_version.blank?
+  elsif !new_resource.current_version.blank?
     destow_current_version
   end
 
   execute "stow_#{name}-#{version}" do
     command "#{stow} #{name}/#{version}"
   end
+
+  new_resource.updated_by_last_action(true)
 end
 
 action :destow do
-  name = @new_resource.name
-  version = @new_resource.version
+  name = new_resource.name
+  version = new_resource.version
 
   execute "destow_#{name}-#{version}" do
     command "#{stow} -D #{name}/#{version}"
   end
+
+  new_resource.updated_by_last_action(true)
 end
