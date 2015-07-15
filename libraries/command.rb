e0ec3345668323baf71a6de8f8a8247b7c71f803
Steven Haddox <steven@haddox.us>
2015-07-15 16:02:32 -0400

    Currently installed version comparison, rubocop cleanup

--- libraries/command.rb
+++ libraries/command.rb
@@ -39,31 +39,50 @@ module StowCookbook
       if ::File.exist?("#{stow_path}/#{creates}") == false
         package_installed = false
       else
-        # TODO Remove this incorrect assumption
-        package_installed = true
         # Determine if the created file points to the proper version of the pkg
-        # TODO
         # Detect lowest level symlink from created_file within stow_path
-        # TODO
-        # Compare symlink to package and version stow'd path
-        # TODO
+        package_installed =
+          !!(::File.realpath(stowed_symlink_path(creates))
+            .match("#{name}#{pkg_delim}#{version}")
+            )
       end
       package_installed
+    end
+
+    # Determine full path for currently stowed package
+    def stowed_symlink_path(creates = nil)
+      stowed_path = nil
+      creates.split('/').each do
+        # Compare symlink version to desired version
+        if ::File.symlink?("#{stow_path}/#{path_elements.join('/')}")
+          # Symlink found, return path
+          stowed_path = path_elements.join('/')
+          break
+        else
+          # Remove lowest path if not a symlink
+          path_elements.pop(1)
+        end
+      end
+
+      stowed_path
     end
 
     # Detect which stow command binary to invoke
     # Order of precedence: -t flag > -d flag > 'stow'
     def stow_command(type = nil)
+      # Default to PATH detected stow
+      command = 'stow'
+      # Override to use the buildout path stow if specified
+      command = "#{stow_buildout_path}/bin/stow" if type == 'buildout'
       if !blank?(stow_target) && stow_target_bin_exists?
+        # Use the target path stow if it exists
         command = "#{stow_target}/bin/stow"
       elsif !blank?(stow_path) && stow_path_bin_exists?
+        # Use the parent dir for stow path if it exists & there's no target
         command = "#{stow_path}/../bin/stow"
       end
 
-      # For the stow recipe it needs the buildout stow directly to stow itself
-      command = "#{stow_buildout_path}/bin/stow" if type == 'buildout'
-
-      command.nil? ? 'stow' : command
+      command
     end
 
     # Set stow command flags
