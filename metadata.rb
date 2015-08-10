4246280e0bca1126fa9bb4db63f79bc978e768e8
Steven Haddox <steven@haddox.us>
2015-08-10 12:52:48 -0400

    v1.0.1

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs GNU Stow & provides stow_package resource'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/stevenhaddox/cookbook-stow'
 issues_url 'https://github.com/stevenhaddox/cookbook-stow/issues'
-version '1.0.0'
+version '1.0.1'
 
 recipe 'stow::default', 'Install GNU stow via package or source (if needed)'
 recipe 'stow::source', 'Install GNU stow via source installation'
